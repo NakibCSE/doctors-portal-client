1048d6d5c8043a9166c03772d7f53a11531c20fb
nakibcse <nakkibcse@gmail.com>
2022-11-17 12:10:04 +0600

    tamilwind configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     themes:[
       {
         doctortheme:{
-          primary: '#0FCFEC'
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          neutral: '#3D4451',
+          "base-100": '#FFFFFF',
         }
       }
     ]
